3212bdaebf52c24f53b4f7f7a7c3ad05976ac3e9
hippo-lore <kn24-o.kovalchuk@nubip.edu.ua>
2025-11-04 13:53:43 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -33,3 +33,46 @@ function removeStudentByName(students, name) {
 }
 
 module.exports = { getTopScoringStudents, getAverageAge, addStudent, removeStudentByName };
+//виконання завдання йоооу
+const students = [
+  { name: "John", age: 20, score: 85 },
+  { name: "Jane", age: 22, score: 92 },
+  { name: "Jim", age: 19, score: 75 },
+  { name: "Jill", age: 21, score: 65 },
+  { name: "Jack", age: 23, score: 95 },
+];
+
+function getTopScoringStudents(students) {
+  const topStudents = [];
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].score > 80) {
+      topStudents.push(students[i].name);
+    }
+  }
+  return topStudents;
+}
+
+function getAverageAge(students) {
+  let totalAge = 0;
+  for (let i = 0; i < students.length; i++) {
+    totalAge += students[i].age;
+  }
+  return totalAge / students.length;
+}
+
+function addStudent(students, student) {
+  students.push(student);
+  return students;
+}
+
+function removeStudentByName(students, name) {
+  const updatedStudents = [];
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].name !== name) {
+      updatedStudents.push(students[i]);
+    }
+  }
+  return updatedStudents;
+}
+
+module.exports = { getTopScoringStudents, getAverageAge, addStudent, removeStudentByName };
