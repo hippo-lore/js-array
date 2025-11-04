974b68c2b7070bb961e01ac099ed2c7ac153ab5e
hippo-lore <kn24-o.kovalchuk@nubip.edu.ua>
2025-11-04 14:03:51 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -5,3 +5,15 @@ function filterAdults(people) {
 }
 
 module.exports = filterAdults;
+//воконання 
+function filterAdults(people) {
+  const adults = [];
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].age >= 18) {
+      adults.push(people[i]);
+    }
+  }
+  return adults;
+}
+
+module.exports = filterAdults;
